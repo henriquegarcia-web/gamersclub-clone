01e7b65fd931ef6373ba8eaa20e100b74931b39c
Henrique Garcia <henriquegarcia.dev@gmail.com>
2024-04-18 07:06:44 -0300

    feat: Composição do Header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,9 +1,230 @@
 import * as S from './styles'
 
+import { LeaderBoard, NavButton, PlayButton, UserInfos } from '@/components'
+
+import { TfiMenu } from 'react-icons/tfi'
+import { Icons } from '@/assets/icons'
+
 interface IHeader {}
 
 const Header = ({}: IHeader) => {
-  return <S.Header></S.Header>
+  return (
+    <S.Header>
+      <TopHeader />
+      <S.MainHeader>
+        <S.MainHeaderPrimary>
+          <S.MainHeaderLogo href="/">
+            <img src="/images/gamersclub-logo.svg" alt="" />
+          </S.MainHeaderLogo>
+          <PlayButton />
+          <LeaderBoard />
+        </S.MainHeaderPrimary>
+        <S.MainHeaderNavBar>
+          <NavBarLobby />
+          <NavBarUser />
+        </S.MainHeaderNavBar>
+      </S.MainHeader>
+    </S.Header>
+  )
 }
 
 export default Header
+
+// ====================================== TOP HEADER
+
+const TopHeader = () => {
+  return <S.TopHeader></S.TopHeader>
+}
+
+// ====================================== NAVBAR LOBBY
+
+const NavBarLobby = () => {
+  const hiddenIcon = (
+    <img src="/icons/hidden.svg" alt="See All Icon" style={{ width: 18 }} />
+  )
+  const skinsIcon = <img src="/icons/skins.svg" alt="Skins Icon" />
+  const championshipIcon = (
+    <img src="/icons/championships.svg" alt="Championships Icon" />
+  )
+  const missionsIcon = (
+    <img src="/icons/missions.svg" alt="Missions Icon" style={{ width: 18 }} />
+  )
+  const academyIcon = <img src="/icons/academy.svg" alt="Academy Icon" />
+  const searchIcon = (
+    <img src="/icons/search.svg" alt="Search Icon" style={{ width: 16 }} />
+  )
+
+  const navbarMenus = [
+    {
+      id: 'navbar_menu_01',
+      icon: skinsIcon,
+      label: 'Skins',
+      hasAlert: false,
+      hasBadge: true,
+      hasCounter: false,
+      selected: false,
+      onClick: () => {}
+    },
+    {
+      id: 'navbar_menu_02',
+      icon: championshipIcon,
+      label: 'Campeonatos',
+      hasAlert: false,
+      hasBadge: true,
+      hasCounter: false,
+      selected: false,
+      onClick: () => {}
+    },
+    {
+      id: 'navbar_menu_03',
+      icon: missionsIcon,
+      label: 'Missões',
+      hasAlert: true,
+      hasBadge: false,
+      hasCounter: false,
+      selected: false,
+      onClick: () => {}
+    },
+    {
+      id: 'navbar_menu_04',
+      icon: academyIcon,
+      label: 'Academy',
+      hasAlert: true,
+      hasBadge: false,
+      hasCounter: false,
+      selected: false,
+      onClick: () => {}
+    },
+    {
+      id: 'navbar_menu_05',
+      icon: searchIcon,
+      label: 'Buscar',
+      hasAlert: false,
+      hasBadge: false,
+      hasCounter: false,
+      selected: false,
+      onClick: () => {}
+    }
+  ]
+
+  return (
+    <S.NavBarLobby>
+      <S.NavBarLobbyDesktopWrapper>
+        {navbarMenus.map((button, index) => (
+          <NavButton
+            key={index}
+            icon={button.icon}
+            label={button.label}
+            hasAlert={button.hasAlert}
+            hasBadge={button.hasBadge}
+            onClick={button.onClick}
+          />
+        ))}
+      </S.NavBarLobbyDesktopWrapper>
+
+      <S.NavBarLobbyMenuMobile>
+        <NavButton
+          icon={hiddenIcon}
+          selected
+          label="Mais (5)"
+          onClick={() => {}}
+        />
+        <S.NavBarLobbyMenuWrapper>
+          {navbarMenus.map((button, index) => (
+            <NavButton
+              key={index}
+              icon={button.icon}
+              label={button.label}
+              onClick={button.onClick}
+            />
+          ))}
+        </S.NavBarLobbyMenuWrapper>
+      </S.NavBarLobbyMenuMobile>
+    </S.NavBarLobby>
+  )
+}
+
+// ====================================== NAVBAR USER
+
+const NavBarUser = () => {
+  const shoppingCartIcon = (
+    <img
+      src={Icons.shoppingCartIcon}
+      alt="Shopping Cart Icon"
+      style={{ height: 20, width: 20 }}
+    />
+  )
+  const upgradeIcon = (
+    <img
+      src={Icons.upgradeIcon}
+      alt="Upgrade Icon"
+      style={{ height: 20, width: 20 }}
+    />
+  )
+  const notifyIcon = (
+    <img
+      src="/icons/notify.svg"
+      alt="Notify Icon"
+      style={{ height: 18, width: 18 }}
+    />
+  )
+  const menuIcon = <TfiMenu />
+
+  const userMenus = [
+    {
+      id: 'user_menu_01',
+      icon: shoppingCartIcon,
+      label: 'Loja',
+      hasAlert: false,
+      hasBadge: false,
+      hasCounter: false,
+      selected: false,
+      onClick: () => {}
+    },
+    {
+      id: 'user_menu_02',
+      icon: upgradeIcon,
+      label: 'Upgrade',
+      hasAlert: false,
+      hasBadge: false,
+      hasCounter: false,
+      selected: true,
+      onClick: () => {}
+    },
+    {
+      id: 'user_menu_03',
+      icon: notifyIcon,
+      label: 'Notificações',
+      hasAlert: false,
+      hasBadge: false,
+      hasCounter: false,
+      selected: false,
+      onClick: () => {}
+    }
+  ]
+
+  return (
+    <S.NavBarUser>
+      {userMenus.map((button) => (
+        <NavButton
+          key={button.id}
+          icon={button.icon}
+          label={button.label}
+          hasAlert={button.hasAlert}
+          hasBadge={button.hasBadge}
+          selected={button.selected}
+          onClick={button.onClick}
+        />
+      ))}
+      <UserInfos />
+      <NavButton
+        icon={menuIcon}
+        label="Menu"
+        hasAlert={false}
+        hasBadge={false}
+        selected={false}
+        onClick={() => {}}
+      />
+    </S.NavBarUser>
+  )
+}
--- client/src/components/Header/styles.ts
+++ client/src/components/Header/styles.ts
@@ -1,5 +1,152 @@
 import styled from 'styled-components'
+import {
+  mainHeaderHeight,
+  responsiveLargeDesktop,
+  topHeaderHeight
+} from '@/utils/styles/globals'
+import { Colors } from '@/utils/styles/colors'
 
-export const Header = styled.div`
+export const Header = styled.header`
   display: flex;
+  flex-direction: column;
+  width: 100%;
+`
+
+// ====================================== MAIN HEADER
+
+export const MainHeader = styled.div`
+  display: flex;
+  width: 100%;
+  height: ${mainHeaderHeight};
+
+  background-color: ${Colors.backgroundSecondary};
+  border-top: 1px solid rgba(250, 249, 247, 0.08);
+  box-shadow: inset 0 -1px hsla(0, 0%, 100%, 0.1),
+    0 8px 16px -4px rgba(0, 0, 0, 0.75);
+`
+
+export const MainHeaderPrimary = styled.div`
+  display: flex;
+
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.24);
+`
+
+export const MainHeaderLogo = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 240px;
+  height: 100%;
+  padding-top: 1px;
+  cursor: pointer;
+  transition: 0.25s;
+
+  img {
+    height: 35%;
+  }
+
+  background-image: linear-gradient(135deg, #3d3d3d, rgba(61, 61, 61, 0));
+  border-bottom: 1px solid #3d3d3d;
+
+  &:hover,
+  &:focus {
+    background-color: #393939;
+    border-bottom: 1px solid #195aff;
+  }
+`
+
+export const MainHeaderNavBar = styled.div`
+  display: flex;
+  flex: 1;
+`
+
+// ====================================== TOP HEADER
+
+export const TopHeader = styled.div`
+  display: flex;
+  width: 100%;
+  height: ${topHeaderHeight};
+`
+
+// ====================================== NAVBAR LOBBY
+
+export const NavBarLobby = styled.div`
+  display: flex;
+
+  button {
+    border-right: 1px solid ${Colors.backgroundQuinary};
+  }
+`
+
+export const NavBarLobbyDesktopWrapper = styled.div`
+  display: flex;
+
+  @media (max-width: ${responsiveLargeDesktop}) {
+    display: none;
+  }
+`
+
+export const NavBarLobbyMenuMobile = styled.div`
+  position: relative;
+  display: none;
+  flex-direction: column;
+
+  @media (max-width: ${responsiveLargeDesktop}) {
+    display: flex;
+  }
+`
+
+export const NavBarLobbyMenuWrapper = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+
+  button {
+    height: 70px;
+
+    p {
+      font-size: 13px;
+    }
+
+    background-color: ${Colors.backgroundSecondary};
+    border: none;
+
+    &:hover {
+      background-color: ${Colors.backgroundTertiary};
+      border: none;
+    }
+  }
+`
+
+// ====================================== NAVBAR USER
+
+export const NavBarUser = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+
+  button {
+    border-left: 1px solid ${Colors.backgroundQuinary};
+  }
+`
+
+export const NavBarUserInfos = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 10px;
+  padding: 0 20px;
+  cursor: pointer;
+  transition: 0.25s;
+
+  border-left: 1px solid ${Colors.backgroundQuinary};
+  border-bottom: 1px solid transparent;
+
+  &:hover,
+  &:focus {
+    background-color: hsla(0, 0%, 100%, 0.025);
+    border-bottom: 1px solid hsla(0, 0%, 100%, 0.5);
+    color: ${Colors.fontWhite};
+  }
 `
