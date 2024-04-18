3932bddc51fdef36ebf1f4901f2f56b382e22e58
Henrique Garcia <henriquegarcia.dev@gmail.com>
2024-04-18 07:06:56 -0300

    feat: Composição da LobbyScreen

--- client/src/screens/LobbyScreen/styles.ts
+++ client/src/screens/LobbyScreen/styles.ts
@@ -1,8 +1,11 @@
 import styled from 'styled-components'
 import { Screen } from '@/utils/styles/globals'
+import { Colors } from '@/utils/styles/colors'
 
 export const TemplateScreen = styled(Screen)`
   display: flex;
+
+  background-color: ${Colors.backgroundPrimary};
 `
 
 // export const OtherTemplateScreen = styled.div`
