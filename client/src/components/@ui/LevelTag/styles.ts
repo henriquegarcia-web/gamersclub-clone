ceae25d96a8dd23e7728e935bbca34c314dfbc95
Henrique Garcia <henriquegarcia.dev@gmail.com>
2024-04-18 08:42:22 -0300

    chore: Ajustando level tag

--- client/src/components/@ui/LevelTag/styles.ts
+++ client/src/components/@ui/LevelTag/styles.ts
@@ -7,15 +7,18 @@ export const LevelTag = styled.div`
   align-items: center;
   width: 20px;
   height: 20px;
-  padding-top: 3px;
+  padding-top: 2px;
   border-radius: 100%;
   overflow: hidden;
 
-  font-size: 15px;
-  line-height: 15px;
+  font-family: 'Poppins', sans-serif;
+  font-size: 12px;
+  line-height: 12px;
   font-weight: 400;
-  letter-spacing: 0.5px;
 
+  border: 1px solid hsla(0, 0%, 100%, 0.2);
+  text-shadow: 0 1px 0 rgba(0, 0, 0, 0.6);
+  box-shadow: 0 5px 3px 0 rgba(0, 0, 0, 0.15);
   color: ${Colors.fontSecondary};
   background-color: #53a18b;
 `
